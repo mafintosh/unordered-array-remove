4f730c12d612bb11a1fd58bd7528bc68ce2b6060
Mathias Buus <mathiasbuus@gmail.com>
2015-12-30 15:34:11 +0100

    fix wrong element being returned

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ module.exports = remove
 function remove (arr, i) {
   if (i >= arr.length) return
   var last = arr.pop()
-  if (i < arr.length) arr[i] = last
+  if (i < arr.length) {
+    var tmp = arr[i]
+    arr[i] = last
+    return tmp
+  }
   return last
 }
