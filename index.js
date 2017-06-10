0293f80745e9219710433333eec8f10892568a8c
Kyle E. Mitchell <kyle@kemitchell.com>
2017-06-09 22:08:48 -0700

    Use !== instead of equivalent <

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = remove
 function remove (arr, i) {
   if (i >= arr.length || i < 0) return
   var last = arr.pop()
-  if (i < arr.length) {
+  if (i !== arr.length) {
     var tmp = arr[i]
     arr[i] = last
     return tmp
