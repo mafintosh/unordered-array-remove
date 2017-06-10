e17a79f14881c7ecb3b98cebc6f562d80ff087c2
Kyle E. Mitchell <kyle@kemitchell.com>
2017-06-09 22:02:10 -0700

    Check element equality before replacing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = remove
 function remove (arr, i) {
   if (i >= arr.length || i < 0) return
   var last = arr.pop()
-  if (i < arr.length) {
+  if (i < arr.length && arr[i] !== last) {
     var tmp = arr[i]
     arr[i] = last
     return tmp
