47e1da696f23a831630e50970ff6e70c09800e05
sricharankrishnan <sricharan.krishnan@gmail.com>
2022-01-05 09:43:29 +0530

    Refactor Improve Validation and Returns
    Added necessary validation processes to ensure that the right type
    of inputs are being received for 'array' and 'index'. Code is now a
    bit more expressive for better readability.
    
    The conditional statement in the end has been changed for better
    understanding of what this actually does.

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-module.exports = remove
+/**
+  * it took me a while to understand the logic behind this. I'd like to 
+  * suggest this be like this. hope you like my suggestion and contribution.
+  *
+  * (BTW I do prefer being a bit more expressive with my coding);
+  *
+  * cheers!
+  **/
 
-function remove (arr, i) {
-  if (i >= arr.length || i < 0) return
-  var last = arr.pop()
-  if (i < arr.length) {
-    var tmp = arr[i]
-    arr[i] = last
-    return tmp
+module.exports = (array, index) => {
+  if (!Array.isArray(array)) {
+    throw new Error(": requires first argument 'array' to be of type javascript array");
   }
-  return last
-}
+  else if (Number.isNaN(index) || typeof index !== "number") {
+    throw new Error(": requires second argument 'index' to be of number type");
+  }
+  else {
+    if (index < 0 || index >= array.length) {
+      return;
+    }
+
+    if (array.length - 1 === index) {
+      return array.pop();
+    }
+    else {
+      /* make the switch */
+      let lastElement = array.pop();
+      let temp = array[index];
+      array[index] = lastElement;
+
+      /* return to caller */
+      return temp;
+    }
+  }
+};
